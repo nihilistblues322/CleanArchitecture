92e9facc3360684b97142270adbaab475ea08108
dimashabdrakhmanov <dimash.abdrakhmanov@brokeree.com>
2025-08-17 20:58:05 +0500

    initial + apartment rich model

---
+++ src/Bookify.Domain/Abstractions/Entity.cs
@@ -0,0 +1,11 @@
+﻿namespace Bookify.Domain.Abstractions;
+
+public abstract class Entity
+{
+    protected Entity(Guid id)
+    {
+        Id = Id;
+    }
+
+    public Guid Id { get; init; }
+}
---
+++ src/Bookify.Domain/Apartments/Apartment.cs
@@ -0,0 +1,34 @@
+﻿using Bookify.Domain.Abstractions;
+
+namespace Bookify.Domain.Apartments;
+
+public sealed class Apartment : Entity
+{
+    public Apartment(
+        Guid id,
+        Name name,
+        Description description,
+        Address address,
+        Money money,
+        Money cleaningFee,
+        List<Amenity> amenities) : base(id)
+    {
+        Name = name;
+        Description = description;
+        Address = address;
+        Price = money;
+        CleaningFee = cleaningFee;
+        Amenities = amenities;
+    }
+
+    public Name Name { get; private set; }
+    public Description Description { get; private set; }
+
+    public Address Address { get; private set; }
+
+    public Money Price { get; private set; }
+    public Money CleaningFee { get; private set; }
+
+    public DateTime? LastBookedOnUtc { get; private set; }
+    public List<Amenity> Amenities { get; private set; } = new();
+}
