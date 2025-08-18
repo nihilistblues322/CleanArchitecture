dd304a8f84dc70ee58e8bf1ab4fe5bbd39113d40
dimashabdrakhmanov <dimash.abdrakhmanov@brokeree.com>
2025-08-18 11:16:10 +0500

    domain events + repo

---
+++ src/Bookify.Domain/Apartments/IApartmentRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace Bookify.Domain.Apartments;
+
+public interface IApartmentRepository
+{
+    Task<Apartment?> GetByIdAsync(int id, CancellationToken cancellationToken = default);
+}
--- src/Bookify.Domain/Users/User.cs
+++ src/Bookify.Domain/Users/User.cs
@@ -1,4 +1,5 @@
 ﻿using Bookify.Domain.Abstractions;
+using Bookify.Domain.Users.Events;
 
 namespace Bookify.Domain.Users;
 
@@ -19,6 +20,7 @@ public sealed class User : Entity
     public static User Create(FirstName firstName, LastName lastName, Email email)
     {
         var user = new User(Guid.NewGuid(), firstName, lastName, email);
+        user.RaiseDomainEvent(new UserCreatedDomainEvent(user.Id));
         return user;
     }
 }
