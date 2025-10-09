920f0376cd6d7ff5fa1e282b12d8a23b95de04ac
dimashabdrakhmanov <dimash.abdrakhmanov@brokeree.com>
2025-10-09 10:55:49 +0500

    application - logging

--- src/Bookify.Application/Bookify.Application/DependencyInjection.cs
+++ src/Bookify.Application/Bookify.Application/DependencyInjection.cs
@@ -1,4 +1,5 @@
-﻿using Bookify.Domain.Bookings;
+﻿using Bookify.Application.Abstractions.Behaviors;
+using Bookify.Domain.Bookings;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Bookify.Application;
@@ -7,7 +8,11 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
-        services.AddMediatR(configuration => { configuration.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly); });
+        services.AddMediatR(configuration =>
+        {
+            configuration.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
+            configuration.AddOpenBehavior(typeof(LoggingBehavior<,>));
+        });
         services.AddTransient<PricingService>();
 
         return services;
