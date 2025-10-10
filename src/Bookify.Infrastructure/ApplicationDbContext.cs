8df428890210596a48e6d87aa4a48bf2b57419b8
dimashabdrakhmanov <dimash.abdrakhmanov@brokeree.com>
2025-10-10 12:09:26 +0500

    domain entity builder to DB

---
+++ src/Bookify.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Bookify.Domain.Abstractions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bookify.Infrastructure;
+
+public sealed class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options), IUnitOfWork
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        base.OnModelCreating(modelBuilder);
+    }
+}
