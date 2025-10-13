069e5cf51d45e2810b32177992cc6276b1392347
dimashabdrakhmanov <dimash.abdrakhmanov@brokeree.com>
2025-10-13 18:05:28 +0500

    connection factory

---
+++ src/Bookify.Infrastructure/Data/SqlConnectionFactory.cs
@@ -0,0 +1,16 @@
+﻿using System.Data;
+using Bookify.Application.Abstractions.Data;
+using Npgsql;
+
+namespace Bookify.Infrastructure.Data;
+
+internal sealed class SqlConnectionFactory(string connectionString) : ISqlConnectionFactory
+{
+    public IDbConnection CreateConnection()
+    {
+        var connection = new NpgsqlConnection(connectionString);
+        connection.Open();
+
+        return connection;
+    }
+}
