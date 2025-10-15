a53b4202bfa3c1e639f7574fb5bb5ef830f3e1ee
dimashabdrakhmanov <dimash.abdrakhmanov@brokeree.com>
2025-10-15 09:39:45 +0500

    publish events

--- src/Bookify.Infrastructure/ApplicationDbContext.cs
+++ src/Bookify.Infrastructure/ApplicationDbContext.cs
@@ -1,13 +1,38 @@
 ﻿using Bookify.Domain.Abstractions;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 
 namespace Bookify.Infrastructure;
 
-public sealed class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options), IUnitOfWork
+public sealed class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, IPublisher publisher) : DbContext(options), IUnitOfWork
 {
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
         base.OnModelCreating(modelBuilder);
+    }
+
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+    {
+        var result = await base.SaveChangesAsync(cancellationToken);
+
+        await PublishDomainEvent();
+
+        return result;
+    }
+
+    private async Task PublishDomainEvent()
+    {
+        var domainEvents = ChangeTracker.Entries<Entity>().Select(entry => entry.Entity).SelectMany(entity =>
+        {
+            var domainEvents = entity.GetDomainEvents();
+            entity.ClearDomainEvents();
+            return domainEvents;
+        }).ToList();
+
+        foreach (var domainEvent in domainEvents)
+        {
+            await publisher.Publish(domainEvent);
+        }
     }
 }
