b1f9b3887acafc7443645b8e3db573d4ab5ba8b2
dimashabdrakhmanov <dimash.abdrakhmanov@brokeree.com>
2026-01-15 16:33:44 +0500

    added api layer

---
+++ src/Bookify.Api/Apartments/ApartmentController .cs
@@ -0,0 +1,20 @@
+﻿using Bookify.Application.Apartments.SearchApartments;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bookify.Api.Apartments;
+
+[ApiController]
+[Route("api/apartments")]
+public class ApartmentController(ISender sender) : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> SearchApartment(DateOnly startDate, DateOnly endDate, CancellationToken cancellationToken)
+    {
+        var query = new SearchApartmentsQuery(startDate, endDate);
+
+        var result = await sender.Send(query, cancellationToken);
+
+        return Ok(result.Value);
+    }
+}
---
+++ src/Bookify.Api/Bookings/ReserveBookingRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace Bookify.Api.Bookings;
+
+public sealed record ReserveBookingRequest(
+    Guid ApartmentId,
+    Guid UserId,
+    DateOnly StartDate,
+    DateOnly EndDate); 
